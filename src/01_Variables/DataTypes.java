4eff27d89694049adf0b0d7977e148558581f718
Enrique-Rosa-Esliceu <erosas@esliceu.net>
2026-09-09 10:42:05 +0200

    Add Variables and DataTypes theory

---
+++ src/01_Variables/DataTypes.java
@@ -0,0 +1,18 @@
+//Tipos de datos primitivos
+
+public class DataTypes {
+    public static void main(String[] args) {
+
+        int myInt = 21; // El dato INT hace servir los datos enteros
+        System.out.println(myInt);
+
+        double myDouble = 1.80; //El dato DOUBLE hace referencia a los decimales
+        System.out.println(myDouble);
+
+        char myChar = 'e'; //El dato CHAR hace servir el tipo de dato caracter
+        System.out.println(myChar);
+
+        boolean myBoolean = true; //El dato BOOLEAN hace referencia si es verdadero o falso
+        System.out.println(myBoolean);
+    }
+}
---
+++ src/01_Variables/Variables.java
@@ -0,0 +1,20 @@
+//Tema 1: Variables y Constantes
+
+public class Variables {
+    public static void main(String[] args) {
+
+        String name = "Enrique";  // La variables NAME és el que guarda o contiene el nombre "Enrique"
+            System.out.println(name);
+
+            int age = 21;  // La variable INT AGE és de tipo entero y almacena el valor de la edad.
+                System.out.println(age);
+
+        //Constantes
+
+        final String EMAIL = "enrique@gmail.com";
+        System.out.println(EMAIL);
+
+        //La palabra FINAL és un candado. Es un dato que és fijo y no se puede cambiar en todo el programa.
+
+    }
+}
