99302ac1360f87a1e6e1f899405f1be300db0af5
Enrique-Rosa-Esliceu <erosas@esliceu.net>
2026-09-17 09:19:20 +0200

    Add Operators theory

--- src/02_Operators/Operators.java
+++ src/02_Operators/Operators.java
@@ -39,18 +39,18 @@ public class Operators {
         //Y (AND)
 
         System.out.println(true && true); //true
-        System.out.println(true && false); //true
-        System.out.println(false && true);
-        System.out.println(false && false);
+        System.out.println(true && false); //false
+        System.out.println(false && true); //false
+        System.out.println(false && false); //false
 
         System.out.println(3 > 2 && 5 == 7 );
 
         // OR (||) → basta con que UNO sea true
         // O (Or)
         System.out.println(true || true); //true
-        System.out.println(true || false); //false
-        System.out.println(false || true);
-        System.out.println(false || false);
+        System.out.println(true || false); //true
+        System.out.println(false || true);  //true
+        System.out.println(false || false); //false
 
         System.out.println(3 > 2 || 5 == 7 );
 
